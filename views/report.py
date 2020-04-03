2a1baa66c6fdd79a89138807a0c7825997841853
matpd <matpd@matpd.tech>
2020-04-02 22:03:39 -0500

    Get all reports call working

--- views/report.py
+++ views/report.py
@@ -1,11 +1,13 @@
 # facebook/views/profile.py
 
 from flask import Blueprint, render_template
+import requests
 
 report = Blueprint('report', __name__)
 
 
-@report.route('/report')
-def timeline():
-    # Do some stuff
-    return 'Local Report'
+@report.route('/report', methods=['GET'])
+def get_all_reports():
+    r = requests.get('https://covid19-server.chrismichael.now.sh/api/v1/AllReports')
+    return r.json()
+
