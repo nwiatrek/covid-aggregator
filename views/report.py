b866d3b0bedbc04ec6453040b2ea42bedcabab9c
matpd <matpd@matpd.tech>
2020-04-05 13:04:37 -0500

    pushing some minor stuff up

--- views/report.py
+++ views/report.py
@@ -7,7 +7,18 @@ report = Blueprint('report', __name__)
 
 
 @report.route('/report', methods=['GET'])
+def do_stuff():
+    return get_report_by_country("USA", get_all_reports()).json()
+
+
 def get_all_reports():
     r = requests.get('https://covid19-server.chrismichael.now.sh/api/v1/AllReports')
     return r.json()
 
+
+def get_report_by_country(country, reports):
+    print(reports.items())
+    for countryReport in reports[0]["table"]:
+        print(countryReport)
+        if countryReport.Country == country:
+            return countryReport
