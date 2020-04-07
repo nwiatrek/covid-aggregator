c11e19d3d06a20d4fd7e6cbd64bddeadc6f41fc7
matpd <matpd@matpd.tech>
2020-04-06 22:05:26 -0500

    Added in logic to get state info

--- views/report.py
+++ views/report.py
@@ -1,6 +1,6 @@
 # facebook/views/profile.py
 
-from flask import Blueprint, render_template
+from flask import Blueprint, make_response, jsonify, request
 import requests
 
 report = Blueprint('report', __name__)
@@ -8,17 +8,25 @@ report = Blueprint('report', __name__)
 
 @report.route('/report', methods=['GET'])
 def do_stuff():
-    return get_report_by_country("USA", get_all_reports()).json()
+    state = request.args.get('state')
+    print(state)
+    return get_report_by_country(state, get_all_reports())
 
 
 def get_all_reports():
-    r = requests.get('https://covid19-server.chrismichael.now.sh/api/v1/AllReports')
+    r = requests.get('https://covid19-server.chrismichael.now.sh/api/v1/CasesInAllUSStates')
     return r.json()
 
 
-def get_report_by_country(country, reports):
+def get_report_by_country(state, reports):
     print(reports.items())
-    for countryReport in reports[0]["table"]:
-        print(countryReport)
-        if countryReport.Country == country:
-            return countryReport
+    if state is None:
+        return reports
+    for stateReport in reports['data'][0]['table']:
+        if stateReport['USAState'] == state:
+            return stateReport
+    return custom_error("Didn't find any data for state: " + state, 404)
+
+
+def custom_error(message, status_code):
+    return make_response(jsonify(message), status_code)
