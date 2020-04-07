4c6f7051076943fdaa3e880d6edd102959c7e15f
ROFRANKLIN <robin.franklin@viasat.com>
2020-04-06 20:35:21 -0500

    pushing up my route

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify
+import requests
 
 app = Flask(__name__)
 
 
+@app.errorhandler(requests.exceptions.RequestException)
+def handle_request_exception(e):
+    return jsonify({"error": str(e)}), 500
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/local-hospital-capacity', methods=['GET'])
+def get_cap():
+    responses = requests.get('https://covid19-server.chrismichael.now.sh/api/v1/AggregatedFacilityCapacityCounty').json()['data'][0]['table']
+    for response in responses:
+        if response['State'] == 'TX' and response['CountyName'] == 'Bexar':
+            tx_response = response
+    if not tx_response:
+        return jsonify({"error": "no info found for Bexar County TX"}), 500
+    return jsonify(tx_response), 200
 
 
 if __name__ == '__main__':
